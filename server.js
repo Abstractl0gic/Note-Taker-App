2382596e5c1ae1d0df7bd33e2e1234e7269cf3dd
abstractl0gic <zwlyons@gmail.com>
2023-10-18 17:20:50 -0500

    update server.js routes

--- server.js
+++ server.js
@@ -1,16 +1,79 @@
 const express = require('express');
-const fs = require('fs-extra');
+const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const dbFilePath = path.join(__dirname, 'db.json');
+const dbFilePath = path.join(__dirname, 'db', 'db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// HTML routes
+// API Routes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        newNote.id = uuidv4();
+        notes.push(newNote);
+        fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+            res.json(notes);
+        });
+    });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    fs.readFile(dbFilePath, 'utf8', (readErr, data) => {
+        if (readErr) {
+            console.error(readErr);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        let notes;
+        try {
+            notes = JSON.parse(data);
+        } catch (parseErr) {
+            console.error(parseErr);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const updatedNotes = notes.filter((note) => note.id !== noteId);
+
+        fs.writeFile(dbFilePath, JSON.stringify(updatedNotes), (writeErr) => {
+            if (writeErr) {
+                console.error(writeErr);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+            res.json(updatedNotes);
+        });
+    });
+});
+
+
+
+// HTML Routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
@@ -19,43 +82,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// API routes
-app.get('/api/notes', (req, res) => {
-    fs.readFile(dbFilePath, 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        res.json(notes);
-    });
-});
-
-app.post('/api/notes', (req, res) => {
-    fs.readFile(dbFilePath, 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        const newNote = req.body;
-        newNote.id = Date.now().toString(); 
-        notes.push(newNote);
-        fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
-            if (err) throw err;
-            res.json(newNote);
-        });
-    });
-});
-
-// DELETE route
-app.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
-    fs.readFile(dbFilePath, 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        const updatedNotes = notes.filter((note) => note.id !== noteId);
-        fs.writeFile(dbFilePath, JSON.stringify(updatedNotes), (err) => {
-            if (err) throw err;
-            res.json({ message: 'Note deleted successfully.' });
-        });
-    });
-});
-
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
+    console.log(`App listening on PORT ${PORT}`);
 });
